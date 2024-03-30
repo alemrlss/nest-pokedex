cd273c5656e4813f7be22beca3f7a2d60940b42d
unknown <Alejandro>
2024-03-29 23:21:50 -0400

    my first commit

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+/* eslint-disable prettier/prettier */
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+  await app.listen(3000);
+
+  console.log(`Application is running on port: 3000...`);
+}
+bootstrap();
---
+++ src/pokemon/dto/create-pokemon.dto.ts
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+import { IsInt, IsPositive, IsString, Min, MinLength } from 'class-validator';
+
+export class CreatePokemonDto {
+  //Entero, Positivo, min 1.
+
+  @IsInt()
+  @IsPositive()
+  @Min(1)
+  no: number;
+
+  @IsString()
+  @MinLength(1)
+  name: string;
+}
