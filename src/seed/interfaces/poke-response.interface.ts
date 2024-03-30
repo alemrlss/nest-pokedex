0d7e6a20bdadd8781c8dc6be2bb351f0312d98d0
unknown <Alejandro>
2024-03-29 23:58:54 -0400

    seed module

---
+++ src/seed/interfaces/poke-response.interface.ts
@@ -0,0 +1,13 @@
+/* eslint-disable prettier/prettier */
+
+export interface PokeResponse {
+  count: number;
+  next: string;
+  previous: null;
+  results: Result[];
+}
+
+export interface Result {
+  name: string;
+  url: string;
+}
